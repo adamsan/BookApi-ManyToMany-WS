4220a2c4cb4cce19ace0ff896592d04c3fe572e9
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-25 00:01:23 +0200

    generate schema from schema.sql, and initalize data from data.sql, use entities just for validation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into book values (1, 'Bible');
+insert into book values (2, 'The Decameron');
+insert into book values (3, 'The Great Gatsby');
+
+insert into author values (1, 'Giovanni Bocacchio');
+insert into author values (2, 'F. Scott Fitzgerald');
