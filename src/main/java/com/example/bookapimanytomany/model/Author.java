ca0edab6543845b778b9570c20956c864d5cbaa2
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-25 00:25:59 +0200

    add list of books to Author entity - 'Basic' attribute type should not be a container error.

--- src/main/java/com/example/bookapimanytomany/model/Author.java
+++ src/main/java/com/example/bookapimanytomany/model/Author.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,4 +18,6 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    
+    private List<Book> books;
 }
