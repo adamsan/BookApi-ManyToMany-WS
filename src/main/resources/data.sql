f072f1c7e50e03a1b2d1de6e1cafd625e3e7cedd
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-25 00:10:29 +0200

    insert data into books table.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,9 @@
 
 insert into book values (1, 'Bible');
 insert into book values (2, 'The Decameron');
-insert into book values (3, 'The Great Gatsby');
+insert into book values (3, 'The Life of Dante');
+insert into book values (4, 'The Great Gatsby');
+insert into book values (5, 'The Curious Case of Benjamin Buttons');
 
 insert into author values (1, 'Giovanni Bocacchio');
 insert into author values (2, 'F. Scott Fitzgerald');
