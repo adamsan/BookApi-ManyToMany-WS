38f24e5e28ec3e0c1f36f417139ad0650a06e68e
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-30 00:14:47 +0200

    add OpenAPI 3/Swagger documentation

--- src/main/java/com/example/bookapimanytomany/BookApiManyToManyApplication.java
+++ src/main/java/com/example/bookapimanytomany/BookApiManyToManyApplication.java
@@ -1,8 +1,20 @@
 package com.example.bookapimanytomany;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Books Api",
+                version = "0.1",
+                description = "Practice Api for building REST Api with many-to-many relationships",
+                license = @License(name = "All rights reserved."),
+                contact = @Contact(name="Jacob Gibbs", email = "noreply@foobar.com")
+        )
+)
 @SpringBootApplication
 public class BookApiManyToManyApplication {
 
--- src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
+++ src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
@@ -2,11 +2,14 @@ package com.example.bookapimanytomany.controller;
 
 import com.example.bookapimanytomany.model.Author;
 import com.example.bookapimanytomany.service.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class AuthorController {
@@ -19,8 +22,22 @@ public class AuthorController {
     }
 
     @GetMapping("/author/{id}")
-    public Optional<Author> findById(@PathVariable("id") Integer id) {
-        return authorService.findById(id);
+    @Operation(
+            summary = "gets all authors",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a list of Author entities."),
+                    @ApiResponse(responseCode = "400", description = "Author by the given id not found.")
+            }
+    )
+    public Author findById(
+            @Parameter(
+                    name = "id",
+                    description = "id of the author",
+                    schema = @Schema(description = "id", minimum = "1", type = "int"),
+                    required = true
+            )
+            @PathVariable("id") Integer id) {
+        return authorService.findById(id).orElseThrow();
     }
 
     @PostMapping("/author")
