2e59b5d7825810dbbb0c0339486d1a4797e37b56
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-29 23:44:55 +0200

    Fix N+1 problem with JPQL query, for other entity, use @EntityGraph

--- src/main/java/com/example/bookapimanytomany/repository/BookDAO.java
+++ src/main/java/com/example/bookapimanytomany/repository/BookDAO.java
@@ -3,7 +3,6 @@ package com.example.bookapimanytomany.repository;
 import com.example.bookapimanytomany.model.Book;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +10,8 @@ import java.util.List;
 @Repository
 public interface BookDAO extends JpaRepository<Book, Integer> {
 
-    @Query("select distinct b from Book b left join fetch b.author a")
-//    @EntityGraph(value = "book_authorship_graph", type = EntityGraph.EntityGraphType.FETCH)
+    //    @Query("select distinct b from Book b left join fetch b.author a") // Other solution for the N+1 query problem
+    @EntityGraph(value = "book_authorship_graph", type = EntityGraph.EntityGraphType.FETCH)
     List<Book> findAll();
 
     Book save(Book book);
